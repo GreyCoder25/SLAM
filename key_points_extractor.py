178e4640280b784079203fc0cc1247307a2eb215
serhiy <serhiylatyuk55@gmail.com>
2019-04-30 00:46:22 +0300

    fix bug with not returning key points from orb.compute() (IT'S IMPORTANT BECAUSE IT CHANGES THE LIST OF KEYPOINTS, CAN REMOVE OR ADD THEM, NOT JUST OUTPUTS THE INPUT KEYPOINTS LIST) works quite fine now

--- key_points_extractor.py
+++ key_points_extractor.py
@@ -1,8 +1,9 @@
 import cv2
+import numpy as np
 
 
 class KeyPointsExtractor(object):
-    def __init__(self, num_points=1000, quality=0.01, min_dist=3):
+    def __init__(self, num_points=3000, quality=0.01, min_dist=3):
         self.num_points = num_points
         self.quality = quality
         self.min_dist = min_dist
@@ -11,8 +12,8 @@ class KeyPointsExtractor(object):
     def extract(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         corners = cv2.goodFeaturesToTrack(gray, self.num_points, qualityLevel=self.quality, minDistance=self.min_dist)
-        key_points = [cv2.KeyPoint(corner[0][0], corner[0][1], _size=40) for corner in corners]
-        descriptors = self.orb.compute(img, key_points)[1]
+        key_points = [cv2.KeyPoint(corner[0][0], corner[0][1], _size=20) for corner in corners]
+        key_points, descriptors = self.orb.compute(img, key_points)
         # return corners, descriptors
         return key_points, descriptors
 
--- main.py
+++ main.py
@@ -4,25 +4,26 @@ import matplotlib.pyplot as plt
 from key_points_extractor import KeyPointsExtractor
 from matcher import Matcher
 
-IMAGE_WIDTH = 600
-IMAGE_HEIGHT = 450
+IMAGE_WIDTH = 1920 // 3
+IMAGE_HEIGHT = 1080 // 3
 
 
 class Viewer(object):
     def __init__(self):
         self.prev_frame_extracts = None
         self.prev_frame = None
-        self.kpe = KeyPointsExtractor(num_points=1000, quality=0.01, min_dist=3)
+        self.kpe = KeyPointsExtractor(num_points=3000, quality=0.01, min_dist=3)
         self.matcher = Matcher()
 
     @staticmethod
     def show_frame(img):
         cv2.imshow('Car driving video', img)
-        print(img.shape)
+        # print(img.shape)
 
     def process_frame(self, img):
         img = cv2.resize(img, (IMAGE_WIDTH, IMAGE_HEIGHT))
         key_points, descriptors = self.kpe.extract(img)
+        print("keypoints: {}".format(len(key_points)))
         # key_points, descriptors = self.kpe.extract_orb(img)
         matches = None
         if self.prev_frame_extracts is not None and self.prev_frame is not None:
@@ -30,12 +31,13 @@ class Viewer(object):
             good_matches = []
             for m, n in matches:
                 if m.distance < 0.75 * n.distance:
-                    good_matches.append(m)
-            good_matches = [(key_points[m.queryIdx], self.prev_frame_extracts['key_points'][m.trainIdx]) for m in good_matches]
+                    kp1 = key_points[m.queryIdx]
+                    kp2 = self.prev_frame_extracts['key_points'][m.trainIdx]
+                    good_matches.append((kp1, kp2))
             # img3 = cv2.drawMatches(img, key_points, self.prev_frame, self.prev_frame_extracts['key_points'],
             #                           good_matches, None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
             # cv2.imshow('matches', img3)
-
+            print("good matches: {}".format(len(good_matches)))
         self.prev_frame_extracts = {'key_points': key_points, 'descriptors': descriptors}
         self.prev_frame = img
 
@@ -44,7 +46,7 @@ class Viewer(object):
                 x, y = np.ravel(key_point.pt).astype(np.int32)
                 x_prev, y_prev = np.ravel(prev_key_point.pt).astype(np.int32)
                 cv2.circle(img, (x, y), radius=1, color=(0, 255, 0), thickness=-1)
-                cv2.circle(img, (x_prev, y_prev), radius=1, color=(0, 255, 0), thickness=-1)
+                # cv2.circle(img, (x_prev, y_prev), radius=1, color=(0, 255, 0), thickness=-1)
                 cv2.line(img, (x, y), (x_prev, y_prev), color=(255, 0, 0), thickness=1)
         return img
 
@@ -52,8 +54,9 @@ class Viewer(object):
 if __name__ == "__main__":
     PATH_TO_VIDEO = 'driving_car_videos/Pexels Videos 1578970.mp4'
     PATH_TO_VIDEO2 = 'driving_car_videos/Pexels Videos 4549.mp4'
+    PATH_TO_VIDEO_GEOHOT = 'driving_car_videos/test_countryroad.mp4'
     viewer = Viewer()
-    cap = cv2.VideoCapture(PATH_TO_VIDEO2)
+    cap = cv2.VideoCapture(PATH_TO_VIDEO_GEOHOT)
 
     while cap.isOpened():
         ret, frame = cap.read()
